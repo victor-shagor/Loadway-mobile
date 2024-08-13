f543583ef271c1a9ac05390a771231d9ac443680
sani yakubu <ysani095@gmail.com>
2024-08-13 18:33:07 +0100

    updated

--- src/screens/bills/index.tsx
+++ src/screens/bills/index.tsx
@@ -1,12 +1,21 @@
-import { SafeAreaView } from "../../components/layout/safeAreaView"
-import {Text} from 'react-native'
+import { SafeAreaView } from "../../components/layout/safeAreaView";
+import { Text, View } from "react-native";
 
-const Bills = () =>{
-    return(
+const Bills = () => {
+  return (
     <SafeAreaView>
-        <Text>Welcome to Bills</Text>
+      <View>
+        <View>
+          <Text>Wallet Balance</Text>
+          <Text>N10,000</Text>
+        </View>
+        <View>
+          <Text>Due Bills</Text>
+          <Text>N20,000</Text>
+        </View>
+      </View>
     </SafeAreaView>
-    )
-}
+  );
+};
 
-export default Bills
+export default Bills;
