d51aa6f87c299bafc13ddc7140069c5501f1fc43
emmanuel98 <salaudeenoluwapelumi98@gmail.com>
2024-08-16 17:39:47 +0100

    Migrated from npm to yarn, tested and confirmed the code is working as expected

--- src/screens/UserAuthentication.tsx/Login.tsx
+++ src/screens/UserAuthentication.tsx/Login.tsx
@@ -5,7 +5,7 @@ import {
   TouchableOpacity,
   KeyboardAvoidingView,
   Platform,
-  ScrollView
+  ScrollView,
 } from "react-native";
 import React, { useEffect, useState } from "react";
 import {
@@ -22,7 +22,6 @@ const group = require("@/src/assets/icons/Group.png");
 const group_one = require("@/src/assets/icons/Group-1.png");
 const group_two = require("@/src/assets/icons/Group-2.png");
 
-
 const Login = () => {
   const [modalVisible, setModalVisible] = useState(false);
 
@@ -30,47 +29,54 @@ const Login = () => {
     setModalVisible(true);
   };
 
+  useEffect(() => {
+    setTimeout(() => {
+      console.log("object");
+      Burnt.toast({ duration: 12, title: "Hello World!", message:"An error occured", from: "top" });
+    }, 1000);
+  }, []);
+
   return (
-    <KeyboardAvoidingView 
-    behavior={Platform.OS === 'ios' ? 'padding' : 'height'}
-      style={{flex: 1, height: '100%'}}
+    <KeyboardAvoidingView
+      behavior={Platform.OS === "ios" ? "padding" : "height"}
+      style={{ flex: 1, height: "100%" }}
     >
       <ScrollView contentContainerStyle={{ flexGrow: 0.1 }}>
-      <View className=" h-full flex-1">
-        <View className="h-[40vh] w-full bg-[#FFF1C6] rounded-b-[70px] relative">
-          <LoadWayLogoDarkWordmark />
-          <View className=" flex-row items-end left-[20%] ml-[-10] absolute bottom-[0.1]">
-            <Image source={group} />
-            <Image source={group_one} />
-            <Image source={group_two} />
-            <Image />
+        <View className=" h-full flex-1">
+          <View className="h-[40vh] w-full bg-[#FFF1C6] rounded-b-[70px] relative">
+            <LoadWayLogoDarkWordmark />
+            <View className=" flex-row items-end left-[20%] ml-[-10] absolute bottom-[0.1]">
+              <Image source={group} />
+              <Image source={group_one} />
+              <Image source={group_two} />
+              <Image />
+            </View>
           </View>
-        </View>
-        <View>
-          <Text className=" text-[#191508] py-10 text-center text-[18px] font-semibold">
-            Welcome back, {"\n"} Let’s make your living experience {"\n"}{" "}
-            awesome!
-          </Text>
-        </View>
-        <View>
-          <EmailInput action="setLoginEmail" />
-          <PasswordInput action="setLoginEmail" />
-        </View>
-        <View>
-          <TouchableOpacity onPress={forgotPasswordHandler}>
-            <Text className=" text-[#F76141] my-2 text-right mr-[6vw]">
-              Forgot Password
+          <View>
+            <Text className=" text-[#191508] py-10 text-center text-[18px] font-semibold">
+              Welcome back, {"\n"} Let’s make your living experience {"\n"}{" "}
+              awesome!
             </Text>
-          </TouchableOpacity>
+          </View>
+          <View>
+            <EmailInput action="setLoginEmail" />
+            <PasswordInput action="setLoginEmail" />
+          </View>
+          <View>
+            <TouchableOpacity onPress={forgotPasswordHandler}>
+              <Text className=" text-[#F76141] my-2 text-right mr-[6vw]">
+                Forgot Password
+              </Text>
+            </TouchableOpacity>
+          </View>
+          <View>
+            <LoginButton />
+          </View>
+          <ResetPasswordModal
+            modalVisible={modalVisible}
+            setModalVisible={setModalVisible}
+          />
         </View>
-        <View>
-          <LoginButton />
-        </View>
-        <ResetPasswordModal
-          modalVisible={modalVisible}
-          setModalVisible={setModalVisible}
-        />
-      </View>
       </ScrollView>
     </KeyboardAvoidingView>
   );
